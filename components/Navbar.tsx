b48c64b65ca27d1a4fb153afb172bd81f15c190d
NicolasEsmite <nicolasemanuelesmitevaldez@gmail.com>
2024-04-30 21:11:34 -0300

    navbar responsivefull

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="fixed flex flex-col items-center justify-center w-full mt-auto h-max bottom-10">
-      <nav>
-        <div className="flex items-center justify-center gap-6 px-6 py-3 rounded-full bg-green-500">
+    <div className="fixed flex flex-col items-center justify-center w-full mt-auto bottom-0 sm:bottom-10">
+      <nav className="flex w-full justify-center">
+        <div className="flex items-center justify-center w-screen px-6 py-3 gap-6  bg-green-600 sm:rounded-full sm:w-fit">
           {menuItems.map((item) => (
             <div key={item.id}>
               <Link href={""}>{item.logo}</Link>
--- data.tsx
+++ data.tsx
@@ -11,17 +11,21 @@ export const menuItems = [
   {
     id: 1,
     logo: <FaHome size={25} className="hover:scale-125" />,
+    name: "Home",
   },
   {
     id: 2,
     logo: <FaUser size={25} className="hover:scale-125" />,
+    name: "AboutMe",
   },
   {
     id: 3,
     logo: <FaUserGraduate size={25} className="hover:scale-125" />,
+    name: "Certifications",
   },
   {
     id: 4,
     logo: <FaUserTie size={25} className="hover:scale-125" />,
+    name: "Experience",
   },
 ];
